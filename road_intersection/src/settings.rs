5e63b39ff23057a4a5f82beb71271e96cc4ad77c
bernaotieno <bernadokumu1@gmail.com>
2025-04-24 00:18:38 +0300

    feat: Add Settings struct initialization method
    Add a new method to the Settings struct that allows for easy initialization of the struct with the provided width, height, vehicle size, gap, and safety distance values. This method calculates various offset values and sets the appearance and stop points for the road intersection.

--- road_intersection/src/settings.rs
+++ road_intersection/src/settings.rs
@@ -33,3 +33,44 @@ pub struct Settings {
     pub dis_vehicle_fourth: Point,
 }
 
+impl Settings {
+    pub fn new(width: i32, height: i32, vehicle: i32, gap: i32, safety_distance: f64) -> Settings {
+        let half_width = width / 2;
+        let half_height = height / 2;
+        let vehicle_width = 2 * vehicle;
+        let offset_road = gap + vehicle_width;
+        let offset_road_s = gap + vehicle;
+
+        Self {
+            width,
+            height,
+            vehicle,
+            gap,
+            safety_distance,
+            offset_road,
+
+            horizontal_road_1: half_height - offset_road,
+            vertical_road_1: half_width - offset_road,
+            horizontal_road_2: half_height + offset_road,
+            vertical_road_2: half_width + offset_road,
+
+            appearance_vehicle_up: Point::new(half_width + (offset_road_s / 2), height),
+            appearance_vehicle_down:Point::new(half_width - 3 * offset_road_s / 2, -vehicle), 
+            appearance_vehicle_left:Point::new(width,  half_height - 3* offset_road_s / 2),
+            appearance_vehicle_right: Point::new(-vehicle, half_height + offset_road_s / 2),
+
+            change_direction_1:Point::new(half_width - 3 * offset_road_s / 2, half_height - 3* offset_road_s / 2),
+            change_direction_2:Point::new(half_width + (offset_road_s / 2), half_height + offset_road_s / 2),
+
+            stop_point_first: Point::new(half_width - 3 * offset_road_s / 2, half_height - offset_road - vehicle),
+            stop_point_second: Point::new(half_width - offset_road - vehicle, half_height + offset_road_s / 2),
+            stop_point_third: Point::new(half_width + offset_road, half_height - 3* offset_road_s / 2),
+            stop_point_fourth: Point::new(half_width + (offset_road_s / 2), half_height + offset_road),
+
+            dis_vehicle_first: Point::new(half_width + (offset_road_s / 2), half_height - offset_road - vehicle),
+            dis_vehicle_second:Point::new(half_width - offset_road - vehicle, half_height - 3* offset_road_s / 2), 
+            dis_vehicle_third:Point::new(half_width + offset_road,  half_height + offset_road_s / 2),
+            dis_vehicle_fourth: Point::new(half_width - 3 * offset_road_s / 2, half_height + offset_road),
+        }
+    }
+}

08532805817cf78004ca3bf9ed1ad9ea6e90a6d0
bernaotieno <bernadokumu1@gmail.com>
2025-04-24 08:37:46 +0300

    Refactor road intersection background color

--- road_intersection/src/main.rs
+++ road_intersection/src/main.rs
@@ -30,7 +30,7 @@ pub fn main() {
     let mut i = 0;
     'running: loop {
         i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(55, 64, 5));
+        canvas.set_draw_color(Color::RGB(35, 35, 35));
         canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
